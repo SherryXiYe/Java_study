34c2b0edb55cc9d937468b95ab94c08a3cdc4f87
SherryXiYe <90816816+SherryXiYe@users.noreply.github.com>
2022-12-26 03:49:09 +0800

    delete

--- java_class/Exercise/exercise202/src/exercise202/Exercise202.java
+++
@@ -1,8 +0,0 @@
-package exercise202;
-
-public class Exercise202 {
-
-	public static void main(String[] args) {
-		System.out.println("Hello,World");
-	}
-}
--- java_class/Exercise/exercise210/src/exercise210/Exercise210.java
+++
@@ -1,9 +0,0 @@
-package exercise210;
-
-public class Exercise210 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
-}
